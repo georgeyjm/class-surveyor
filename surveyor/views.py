34ed05e94d3a3de3b3ce97137e962f30deeb7d52
George Yu <yudumpling@gmail.com>
2020-03-20 18:57:50 +0800

    Improve import format (remove wildcard imports)

--- surveyor/views.py
+++ surveyor/views.py
@@ -8,8 +8,8 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.security import generate_password_hash
 
 from . import app, db, login_manager
-from .models import *
-from .helper import *
+from .models import Teacher, Class, User, Feedback
+from .helper import ykps_auth
 
 
 
